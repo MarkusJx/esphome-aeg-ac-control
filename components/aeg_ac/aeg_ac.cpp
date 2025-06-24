742c1b37e40df9aebab84890307f34e5e9ddb6c6
MarkusJx <28785953+MarkusJx@users.noreply.github.com>
2025-06-24 18:42:19 +0200

    chore: add mode auto

--- components/aeg_ac/aeg_ac.cpp
+++ components/aeg_ac/aeg_ac.cpp
@@ -45,6 +45,7 @@ climate::ClimateTraits AegAC::traits() {
 
   traits.set_supported_modes({
       climate::CLIMATE_MODE_OFF,
+      climate::CLIMATE_MODE_AUTO,
       climate::CLIMATE_MODE_DRY,
       climate::CLIMATE_MODE_COOL,
       climate::CLIMATE_MODE_FAN_ONLY,
